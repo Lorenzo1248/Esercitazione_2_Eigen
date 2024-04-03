ba8fa1f92329cd5d4e3a2071238161864be13f4e
Lorenzo1248 <161575619+Lorenzo1248@users.noreply.github.com>
2024-04-03 14:41:12 +0200

    Esercizio 2-ri-svolto

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,55 +1,98 @@
 #include <iostream>
-#include <Eigen/Dense>
+#include "Eigen/Eigen"
 
 using namespace std;
 using namespace Eigen;
 
-int main() {
+/// \brief Test the real solution of system Ax = b
+/// \return the relative error for PALU solver
+/// \return the relative error for QR solver
+void TestSolution(const MatrixXd& A,
+                  const VectorXd& b,
+                  const VectorXd& solution,
+                  double& errRelPALU,
+                  double& errRelQR);
 
-    //Definisco le matrici A e b per ogni sistema
-    Matrix2d A1, A2, A3;
-    Vector2d b1, b2, b3;
+/// \brief Solve linear system with PALU
+/// \return the solution
+VectorXd SolveSystemPALU(const MatrixXd& A,
+                         const VectorXd& b);
 
-    A1 << 5.547001962252291e-01, -3.770900990025203e-02, 8.320502943378437e-01, -9.992887623566787e-01;
-    b1 << -5.169911863249772e-01, 1.672384680188350e-01;
+/// \brief Solve linear system with PALU
+/// \return the solution
+VectorXd SolveSystemQR(const MatrixXd& A,
+                       const VectorXd& b);
 
-    A2 << 5.547001962252291e-01, -5.540607316466765e-01, 8.320502943378437e-01, -8.324762492991313e-01;
-    b2 << -6.394645785530173e-04, 4.259549612877223e-04;
+int main()
+{
+    Vector2d solution(-1.0000e+0, -1.0000e+00);
 
-    A3 << 5.547001962252291e-01, -5.547001955851905e-01, 8.320502943378437e-01, -8.320502947645361e-01;
-    b3 << -6.400391328043042e-10, 4.266924591433963e-10;
+    Matrix2d A1 {{5.547001962252291e-01, -3.770900990025203e-02},{8.320502943378437e-01, -9.992887623566787e-01}};   //= Matrix2d::Zero();
+    Vector2d b1 {-5.169911863249772e-01, 1.672384680188350e-01};   //= Vector2d::Zero();
 
-    //Soluzione con decomposizione PALU
-    //Vector2d x1, x2, x3;(posso anche chiamarlo una sola volta per tutto l'esercizio)
-    Vector2d x1_PALU = A1.partialPivLu().solve(b1);
-    Vector2d x2_PALU = A2.partialPivLu().solve(b2);
-    Vector2d x3_PALU = A3.partialPivLu().solve(b3);
+    double errRel1PALU, errRel1QR;
+    TestSolution(A1, b1, solution, errRel1PALU, errRel1QR);
 
-    //Calcolo gli errori relativi
-    double errore_rel1 = (A1 * x1_PALU - b1).norm() / b1.norm();
-    double errore_rel2 = (A2 * x2_PALU - b2).norm() / b2.norm();
-    double errore_rel3 = (A3 * x3_PALU - b3).norm() / b3.norm();
+    if (errRel1PALU < 1e-15 && errRel1QR < 1e-15)
+        cout<< scientific<< "1 - "<< "PALU: "<< errRel1PALU<< " QR: "<< errRel1QR<< endl;
+    else
+    {
+        cerr<< "1 - Wrong system solution found"<< endl;
+        return -1;
+    }
 
-    cout << "Errori relativi con la decomposizione PALU:" << endl;
-    cout << "Sistema 1: " << errore_rel1 << endl;
-    cout << "Sistema 2: " << errore_rel2 << endl;
-    cout << "Sistema 3: " << errore_rel3 << endl;
+    Matrix2d A2 {{5.547001962252291e-01, -5.540607316466765e-01},{8.320502943378437e-01,-8.324762492991313e-01}};   //= Matrix2d::Zero();
+    Vector2d b2 {-6.394645785530173e-04, 4.259549612877223e-04};   //= Vector2d::Zero();
 
-    //Soluzione con decomposizione QR
-    Vector2d x1_QR = A1.colPivHouseholderQr().solve(b1);
-    Vector2d x2_QR = A2.colPivHouseholderQr().solve(b2);
-    Vector2d x3_QR = A3.colPivHouseholderQr().solve(b3);
+    double errRel2PALU, errRel2QR;
+    TestSolution(A2, b2, solution, errRel2PALU, errRel2QR);
 
-    //Calcolo gli errori relativi
-    errore_rel1 = (A1 * x1_QR - b1).norm() / b1.norm();
-    errore_rel2 = (A2 * x2_QR - b2).norm() / b2.norm();
-    errore_rel3 = (A3 * x3_QR - b3).norm() / b3.norm();
+    if (errRel2PALU < 1e-12 && errRel2QR < 1e-12)
+        cout<< scientific<< "2 - "<< "PALU: "<< errRel2PALU<< " QR: "<< errRel2QR<< endl;
+    else
+    {
+        cerr<< "2 - Wrong system solution found"<< endl;
+        return -1;
+    }
 
-    cout << "Errori relativi con la decomposizione QR:" << endl;
-    cout << "Sistema 1: " << errore_rel1 << endl;
-    cout << "Sistema 2: " << errore_rel2 << endl;
-    cout << "Sistema 3: " << errore_rel3 << endl;
+    Matrix2d A3 {{5.547001962252291e-01, -5.547001955851905e-01},{8.320502943378437e-01,-8.320502947645361e-01}};   //= Matrix2d::Zero();
+    Vector2d b3 {-6.400391328043042e-10, 4.266924591433963e-10};   //= Vector2d::Zero();
+
+    double errRel3PALU, errRel3QR;
+
+    TestSolution(A3, b3, solution, errRel3PALU, errRel3QR);
+
+    if (errRel3PALU < 1e-5 && errRel3QR < 1e-5)
+        cout<< scientific<< "3 - "<< "PALU: "<< errRel3PALU<< " QR: "<< errRel3QR<< endl;
+    else
+    {
+        cerr<< "3 - Wrong system solution found"<< endl;
+        return -1;
+    }
 
     return 0;
 }
 
+void TestSolution(const MatrixXd& A,
+                  const VectorXd& b,
+                  const VectorXd& solution,
+                  double& errRelPALU,
+                  double& errRelQR)
+{
+    errRelPALU = (solution - SolveSystemPALU(A,b)).norm() / solution.norm();     //numeric_limits<double>::max();
+    errRelQR = (solution - SolveSystemQR(A,b)).norm() / solution.norm();     //numeric_limits<double>::max();
+}
+
+VectorXd SolveSystemPALU(const MatrixXd& A,
+                         const VectorXd& b)
+{
+    VectorXd x = A.fullPivLu().solve(b);
+    return x;     //Vector2d::Zero();
+}
+
+VectorXd SolveSystemQR(const MatrixXd& A,
+                       const VectorXd& b)
+{
+    VectorXd x = A.householderQr().solve(b);
+    return x;     //Vector2d::Zero();
+}
